8f37cedfc35504438e5d6ffc92ff561077ff50b3
Riley Paul <rileypaul96@gmail.com>
2025-08-13 15:15:41 -0600

    update item handlers

--- src/actions/items/items.inputs.ts
+++ src/actions/items/items.inputs.ts
@@ -1,17 +1,17 @@
 import { zItemInsert } from "@/lib/types";
 import { z } from "zod";
 
-const itemInputs = {
-  getAll: z.any(),
-  create: zItemInsert.omit({ userId: true }),
-  update: zItemInsert.omit({ userId: true }).partial().required({ id: true }),
-  duplicate: z.object({ itemId: z.string() }),
-  remove: z.object({ itemId: z.string() }),
-  getListsIncluded: z.object({ itemId: z.string() }),
-  imageUpload: z.object({
-    itemId: z.string(),
-    imageFile: z.instanceof(File).optional(),
-    removeImageFile: z.boolean().optional(),
-  }),
-};
-export default itemInputs;
+export const getAll = z.any();
+export const create = zItemInsert.omit({ userId: true });
+export const update = zItemInsert
+  .omit({ userId: true })
+  .partial()
+  .required({ id: true });
+export const duplicate = z.object({ itemId: z.string() });
+export const remove = z.object({ itemId: z.string() });
+export const getListsIncluded = z.object({ itemId: z.string() });
+export const imageUpload = z.object({
+  itemId: z.string(),
+  imageFile: z.instanceof(File).optional(),
+  removeImageFile: z.boolean().optional(),
+});
