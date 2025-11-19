a12a73b4557ab1a29706b92aa350094daa27dce2
Riley Paul <rileypaul96@gmail.com>
2025-11-19 08:08:50 -0700

    move script files

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,6 @@
 import { beforeAll } from "vitest";
 import { execSync } from "child_process";
-import { deleteAllData } from "@/db/scripts";
+import deleteAllData from "@/db/scripts/delete-all-data";
 import { seedTestData } from "@/tests/fixtures";
 
 beforeAll(async () => {
